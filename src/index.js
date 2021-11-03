d1794bf5bd6fc64c94c8fd17e80edfb1d0d3df59
asnewman <ashznewman@gmail.com>
2021-11-03 08:04:18 +0000

    Bomb collision

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ const ctx = canvas.getContext("2d");
 const CELL_SIZE = 50;
 const GRID_SIZE_X = 18;
 const GRID_SIZE_Y = 12;
-const MAX_BOMBS = 3;
-const BOMB_TIMER = 1000;
+const MAX_BOMBS = 2;
+const BOMB_TIMER = 1500;
 
 let player = {
   x: 0,
   y: 0,
-  activeBombs: 0
+  activeBombs: 0,
 };
 
 let bombLocs = new Set();
@@ -46,8 +46,6 @@ window.addEventListener("keydown", (e) => {
 });
 
 const dropBomb = () => {
-  console.log(player);
-
   if (player.activeBombs >= MAX_BOMBS) {
     return;
   }
@@ -55,25 +53,46 @@ const dropBomb = () => {
   const newBomb = { x: player.x, y: player.y };
 
   bombLocs.add(newBomb);
+  console.log(bombLocs);
   player = { ...player, activeBombs: player.activeBombs + 1 };
   render();
 
   setTimeout(() => {
-    bombLocs.delete(newBomb);
+    //bombLocs.delete(newBomb);
     player = { ...player, activeBombs: player.activeBombs - 1 };
     render();
   }, BOMB_TIMER);
 };
 
-const renderNewPlayer = (newPlayer) => {
-  if (
+const hasCollision = (newPlayer) => {
+  console.log(bombLocs);
+  console.log({ x: newPlayer.x, y: newPlayer.y });
+  console.log(bombLocs.has({ x: newPlayer.x, y: newPlayer.y }));
+  return (
     newPlayer.x >= GRID_SIZE_X ||
     newPlayer.y >= GRID_SIZE_Y ||
     newPlayer.x === -1 ||
-    newPlayer.y === -1
-  ) {
-    return;
-  }
+    newPlayer.y === -1 ||
+    setHas(bombLocs, { x: newPlayer.x, y: newPlayer.y })
+  );
+};
+
+/**
+ * Horrible
+ */
+const setHas = (s, value) => {
+  let has = false;
+  s.forEach(e => {
+    if (JSON.stringify(e) === JSON.stringify(value)) {
+      has = true;
+    }
+  });
+
+  return has;
+}
+
+const renderNewPlayer = (newPlayer) => {
+  if (hasCollision(newPlayer)) return;
 
   player = { ...newPlayer };
   render();
